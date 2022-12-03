a4e9a4522a88bd82fa6600a0fa2c91b8b1d5ef41
miku0129 <svartkatt39@gmail.com>
2022-12-03 18:31:23 +0100

    Revert "commit from container"
    This reverts commit 7b9858c1fa7b221439aa967a11bf5a71e3d60727.

--- .devcontainer/postCreateCommand.sh
+++
@@ -1,7 +0,0 @@
-echo "hello world!"
-
-git config --global user.email 'svartkatt39@gmail.com'
-git config --global user.name 'miku0129'
-
-mkdir -p ~/.ssh/
-curl -s -o ~/.ssh/id_ed25519 "https://github.com/$(git config --global --get user.name).keys"
