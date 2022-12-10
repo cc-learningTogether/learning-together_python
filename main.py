031eb4158fd68ea0482d227e1f970ee2bcb384e4
Mattia <59755795+matt185@users.noreply.github.com>
2022-12-10 22:59:24 +0900

    create router function

--- main.py
+++ main.py
@@ -4,13 +4,7 @@ from dotenv import load_dotenv
 
 from app import create_app
 
-from routes.home import home_route
-from routes.signup import signup_route
-from routes.signin import signin_route
-from routes.logout import logout_route
-from routes.forgot_password import forgot_password_route
-from routes.change_password import change_psw_route
-from routes.settings import settings_route
+from routes.router import initialize_routes
 
 # load_dotenv make possible to use a .env file for store the environment variable
 load_dotenv()
@@ -19,19 +13,8 @@ load_dotenv()
 app = create_app()
 
 # Register the blueprint (routes)
-app.register_blueprint(home_route)
 
-app.register_blueprint(signup_route)
-
-app.register_blueprint(signin_route)
-
-app.register_blueprint(logout_route)
-
-app.register_blueprint(forgot_password_route)
-
-app.register_blueprint(change_psw_route)
-
-app.register_blueprint(settings_route)
+initialize_routes(app)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8080))
---
+++ routes/router.py
@@ -0,0 +1,23 @@
+from routes.home import home_route
+from routes.signup import signup_route
+from routes.signin import signin_route
+from routes.logout import logout_route
+from routes.forgot_password import forgot_password_route
+from routes.change_password import change_psw_route
+from routes.settings import settings_route
+
+
+def initialize_routes(app):
+    app.register_blueprint(home_route)
+
+    app.register_blueprint(signup_route)
+
+    app.register_blueprint(signin_route)
+
+    app.register_blueprint(logout_route)
+
+    app.register_blueprint(forgot_password_route)
+
+    app.register_blueprint(change_psw_route)
+
+    app.register_blueprint(settings_route)
