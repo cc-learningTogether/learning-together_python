b9687872046bb0893ae3b5bcb6aa3247f7dd7908
miku0129 <svartkatt39@gmail.com>
2022-12-20 16:56:16 +0100

    implement date picker

--- routes/home.py
+++ routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request
 from flask_login import current_user
 from jinja2 import TemplateNotFound
 from utils.constants import YEAR, SITE_NAME
@@ -6,11 +6,22 @@ from utils.constants import YEAR, SITE_NAME
 home_route = Blueprint('home', __name__, template_folder="routes")
 
 
-@home_route.route('/')
+@home_route.route('/', methods=['GET', 'POST'])
 def home():
-    try:
-        if current_user.is_authenticated:
-            return render_template('index.html', year=YEAR, name=SITE_NAME)
-        return render_template('index.html', year=YEAR)
-    except TemplateNotFound:
-        return abort(404)
+    if request.method == 'POST':
+        start = request.form["start"]
+        end = request.form["end"]
+        try:
+            if current_user.is_authenticated:
+                return render_template('index.html', year=YEAR, name=SITE_NAME, start=start, end=end)
+            return render_template('index.html', year=YEAR)
+        except TemplateNotFound:
+            return abort(404)
+
+    else:
+        try:
+            if current_user.is_authenticated:
+                return render_template('index.html', year=YEAR, name=SITE_NAME)
+            return render_template('index.html', year=YEAR)
+        except TemplateNotFound:
+            return abort(404)
