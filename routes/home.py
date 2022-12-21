8de068716c17ca83961a430d49a4d18a5c63faee
miku0129 <svartkatt39@gmail.com>
2022-12-21 20:48:58 +0100

    implement validation for datetimepicker

--- routes/home.py
+++ routes/home.py
@@ -4,15 +4,33 @@ from jinja2 import TemplateNotFound
 from utils.constants import YEAR, SITE_NAME
 
 from flask_wtf import FlaskForm
-from wtforms.fields import DateTimeField
+from wtforms import StringField, ValidationError
+from wtforms.validators import DataRequired
+from datetime import datetime
+
 
 home_route = Blueprint('home', __name__, template_folder="routes")
 
 
 class DateTimeForm_start(FlaskForm):
-    dt_start = DateTimeField()
+    dt_start = StringField("datetime_start", validators=[DataRequired()])
+
+    def validate_dt_start(self, dt_start):
+        # incorrect input 
+        date = datetime.strptime(dt_start.data, "%Y/%m/%d %H:%M")
+        if ( date - datetime.now() ).total_seconds() < 0 :
+            raise ValidationError("Chose later than today")
+
+
 class DateTimeForm_finish(FlaskForm):
-    dt_finish = DateTimeField()
+    dt_finish = StringField("datetime_finish", validators=[DataRequired()])
+
+    def validate_dt_finish(self, dt_finish):
+        # incorrect input 
+        date = datetime.strptime(dt_finish.data, "%Y/%m/%d %H:%M")
+        if ( date - datetime.now() ).total_seconds() < 0 :
+            raise ValidationError("Chose later than today")
+
 
 
 @home_route.route('/', methods=['GET', 'POST'])
@@ -21,23 +39,30 @@ def home():
     dtf_finish = DateTimeForm_finish()
 
     if request.method == 'POST':
-        dt_start_val= request.form['dt_start']
-        dt_finish_val = request.form['dt_finish']
-        dt_start_val_type = type(dt_start_val)
-        try:
-            if current_user.is_authenticated:
-                return render_template('index.html', year=YEAR, name=SITE_NAME, 
-                form_start=dtf_start, form_finish=dtf_finish, 
-                date_start=dt_start_val, date_finish=dt_finish_val, date_start_type=dt_start_val_type)
+        if dtf_start.validate_on_submit() and dtf_finish.validate_on_submit():
+            dt_start_val= request.form['dt_start']
+            dt_finish_val = request.form['dt_finish']
 
-            return render_template('index.html', year=YEAR, form_start=dtf_start, form_finish=dtf_finish)
-        except TemplateNotFound:
-            return abort(404)
+            #validate two inputs
+            first = datetime.strptime(dt_start_val, "%Y/%m/%d %H:%M")
+            second = datetime.strptime(dt_finish_val, "%Y/%m/%d %H:%M")
+            if ( second - first ).total_seconds() < 0 :
+                return render_template('index.html', year=YEAR, 
+                form_start=dtf_start, form_finish=dtf_finish, message="Validation error")
+
+            try:
+                if current_user.is_authenticated:
+                    return render_template('index.html', year=YEAR, name=SITE_NAME, 
+                    form_start=dtf_start, form_finish=dtf_finish, 
+                    date_start=dt_start_val, date_finish=dt_finish_val, message="Success")
+            except TemplateNotFound: return abort(404)
+
+
+        return render_template('index.html', year=YEAR, form_start=dtf_start, form_finish=dtf_finish, message="Validation error")
 
     else:
         try:
             if current_user.is_authenticated:
                 return render_template('index.html', year=YEAR, name=SITE_NAME, form_start=dtf_start, form_finish=dtf_finish)
             return render_template('index.html', year=YEAR, form_start=dtf_start, form_finish=dtf_finish)
-        except TemplateNotFound:
-            return abort(404)
+        except TemplateNotFound: return abort(404)
