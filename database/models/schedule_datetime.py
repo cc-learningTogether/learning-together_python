bc385228ecab0b72b171b188c34645e3b63d5147
miku0129 <svartkatt39@gmail.com>
2022-12-23 14:36:41 +0100

    modefied user_booking_slot as foreign key/nullable

--- database/models/schedule_datetime.py
+++ database/models/schedule_datetime.py
@@ -8,6 +8,6 @@ class ScheduleDatetime(db.Model):
     start_at = db.Column(db.DateTime(timezone=True), nullable=False)
     finish_at = db.Column(db.DateTime(timezone=True), nullable=False)
     user_opening_slot = db.Column(db.Integer, db.ForeignKey('users_profiles.id'), nullable=False)
-    user_booking_slot = db.Column(db.Integer)
+    user_booking_slot = db.Column(db.Integer, db.ForeignKey('users_profiles.id'), nullable=True)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
