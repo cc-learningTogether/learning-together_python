ba9c745c9785a56a264b51406527f2296f2c0f23
miku0129 <svartkatt39@gmail.com>
2023-02-20 17:41:50 +0100

    take a snap shot

--- routes/book.py
+++ routes/book.py
@@ -16,6 +16,7 @@ book_route = Blueprint('book', __name__, template_folder="routes")
 def book():
     if request.method == "POST": 
         try:
-            return render_template('book.html')
+            id = request.form["val"]
+            return render_template('book.html', result = id)
         except TemplateNotFound: return abort(404)
     return render_template('book.html')
